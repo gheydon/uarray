7f546945a41a3a405525ded1da3ff3bc35b71fdd
Gordon Heydon <gordon@heydon.com.au>
2013-04-02 13:11:04 +1100

    Add more tests

--- tests/RocketSoftware/u2/uArrayTest.php
+++ tests/RocketSoftware/u2/uArrayTest.php
@@ -109,4 +109,108 @@ class uArrayTest extends \PHPUnit_Framework_TestCase {
 
     return $v;
   }
+  
+  public function testInsOnZero() {
+    $v = new RocketSoftware\u2\uArray('1', array('delimiter' => "\xfd"));
+
+    $v->ins('6', 6);
+    $this->assertEquals("1\xfd\xfd\xfd\xfd\xfd6", (string)$v);
+
+    return $v;
+  }
+  
+  public function testCounter() {
+    $v = new RocketSoftware\u2\uArray("1\xfd2\xfd3\xfd4\xfd5\xfd6\xfd7\xfd8\xfd9");
+    
+    $this->assertEquals(9, count($v));
+    
+    $v[] = '10';
+    
+    $this->assertEquals(10, count($v));
+    
+    $v[20] = '20';
+    $this->assertEquals(20, count($v));
+    
+    return $v;
+  }
+  
+  public function testEmptyCount() {
+    $v = new RocketSoftware\u2\uArray("");
+
+    $this->assertEquals(0, count($v));
+    
+    return $v;
+  }
+  
+  public function testSingleCount() {
+    $v = new RocketSoftware\u2\uArray("1");
+
+    $this->assertEquals(1, count($v));
+    
+    return $v;
+  }
+  
+  public function testInterator() {
+    $v = new RocketSoftware\u2\uArray("1\xfd2\xfd3\xfd4\xfd5\xfd6\xfd7\xfd8\xfd9");
+    
+    $i = $j = 1;
+    foreach ($v as $key => $value) {
+      $this->assertEquals($i++, (string)$value);
+      $this->assertEquals($j++, $key);
+    }
+    
+    return $v;
+  }
+  
+  public function testAddNull() {
+    $v = new RocketSoftware\u2\uArray("1\xfd2\xfd3\xfd4\xfd5\xfd6\xfd7\xfd8\xfd9");
+    
+    $v[20] = '';
+    $this->assertEquals("1\xfd2\xfd3\xfd4\xfd5\xfd6\xfd7\xfd8\xfd9", (string)$v);
+    
+    $v[9] = '';
+    $this->assertEquals("1\xfd2\xfd3\xfd4\xfd5\xfd6\xfd7\xfd8", (string)$v);
+    
+    return $v;
+  }
+  
+  /**
+   * @depends testAddNull
+   */
+  public function testSearch($v) {
+    $this->assertEquals(5, $v->search(5));
+    $this->assertEquals('5', (string)$v[$v->search(5)]);
+
+    return $v;
+  }
+
+  /**
+   * @depends testAddNull
+   */
+  public function testUniqueSearch($v) {
+    $v[14] = 'abc';
+    
+    $this->assertEquals(14, $v->searchUnique('abc'));
+    
+    $this->assertFalse($v->searchUnique('def'));
+    
+    $v[15] = 'abc';
+    $this->assertEquals(15, $v->searchUnique('abc'));
+    
+    return $v;
+  }
+  
+  public function testUniqueSearchOnEmpty() {
+    $v = new RocketSoftware\u2\uArray();
+    
+    $this->assertFalse($v->searchUnique('def'));
+    
+    return $v;
+  }
+
+  public function testIssetZero() {
+    $v = new RocketSoftware\u2\uArray("1");
+    
+    $this->assertTrue(isset($v[0]));
+  }
 }
